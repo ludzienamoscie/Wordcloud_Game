ba1e38984cf9784a284a9cefb5de19bee4ea5a0e
236527 Frydlewska Maja <236527@edu.p.lodz.pl>
2022-05-11 22:14:07 +0200

    Added style to the scoreScreen, additional marking of choices and comments

--- Wordcloud_Game/app.js
+++ Wordcloud_Game/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+let allAnswers = 0
+let rightAnswers = 0
+let selectedIds = []
+
 // Questions and right answers
 let questionsData =
 [
@@ -68,11 +72,9 @@ let questionsData =
     ]
 }
 ]
-//let json = JSON.parse(questionsData)
-
 
 // Checking if the user entered their nickname
-function checkGuess() {
+function checkName() {
     if (!document.getElementById("nicknameInput").value) {
         alert("Please enter your nickname. ")
         return false
@@ -80,33 +82,9 @@ function checkGuess() {
     return true
 }
 
-let allAnswers = 0
-let rightAnswers = 0
-let selectedIds = []
-
-function randomPosition(length) {
-    let positions = []
-    while (positions.length < length) {
-        let row = Math.random()
-        let col = Math.random()
-        // If this position is not yet included, we can add it
-        let duplicate = false
-        positions.forEach((element, index, array) => {
-            // 10% difference between words
-            if (Math.abs(element.x - col) < 0.1 && Math.abs(element.y - row) < 0.1) {
-                duplicate = true
-                return
-            }
-        });
-        if (!duplicate) {
-            positions.push({x: col, y: row})
-        }
-    }
-    return positions
-}
-
+// Submitting user's nickname and changing the view to the game screen
 function submitNickname() {
-    if (!checkGuess()) {
+    if (!checkName()) {
         return
     }
     let title = document.getElementById("title")
@@ -123,43 +101,9 @@ function submitNickname() {
     renderGame()
 }
 
-function wordCallback(id) {
-    // Removing previously selected element
-    if (selectedIds.includes(id)) {
-        selectedIds.splice(selectedIds.indexOf(id), 1)
-        document.getElementById("word" + id).style.color = "black"
-    } else {
-        document.getElementById("word" + id).style.color = "#8c92ac"
-        selectedIds.push(id)
-    }
-}
-
-function checkAnswers() {
-    let points = 0
-    for (let index in selectedIds) {
-        let id = selectedIds[index]
-        if (rightAnswers.includes(allAnswers[id])) {
-            document.getElementById("word" + id).style.color = "#8fbc8f"
-            points += 2
-        } else {
-            document.getElementById("word" + id).style.color = "#cc3333"
-            points -= 1
-        }
-    }
-    for (let index in rightAnswers) {
-        let id = allAnswers.indexOf(rightAnswers[index])
-        if (!selectedIds.includes(id)) {
-            document.getElementById("word" + id).style.color = "#8fbc8f"
-            points -= 1
-        }
-    }
-    let button = document.getElementById("answersButton")
-    button.innerHTML = "finish game"
-    button.setAttribute("onclick", "submitAnswers()")
-    document.getElementById("score").innerHTML = points + " points"
-}
-
+// Choosing the game
 function renderGame() {
+    // Choosing random question set
     let choice = Math.floor(Math.random() * questionsData.length)
     let question = questionsData[choice].question
     question = question.charAt(0).toUpperCase() + question.slice(1)
@@ -168,10 +112,12 @@ function renderGame() {
     rightAnswers = questionsData[choice].good_words
     let wordPositions = randomPosition(allAnswers.length)
 
+    // Creating nodes for words 
     for (let i = 0; i < wordPositions.length; i++) {
         let wordNode = document.createElement('label');
-        wordNode.setAttribute("id", "word"+i); 
-        wordNode.setAttribute("onclick", "wordCallback("+i+")");     
+        wordNode.setAttribute("id", "word" + i)
+        wordNode.setAttribute("onclick", "wordCallback(" + i + ")")
+        wordNode.setAttribute("class", "word")
         wordNode.style.position = "absolute"
         document.getElementById("gameBoard").style.position = "relative"
         wordNode.style.fontWeight = "bold"
@@ -183,6 +129,74 @@ function renderGame() {
     }
 }
 
+// Assigning random positions for words
+function randomPosition(length) {
+    let positions = []
+    while (positions.length < length) {
+        let row = Math.random()
+        let col = Math.random()
+        let duplicate = false
+        positions.forEach((element, index, array) => {
+            // 10% difference between words must remain, otherwise - it's a duplicate
+            if (Math.abs(element.x - col) < 0.1 && Math.abs(element.y - row) < 0.1) {
+                duplicate = true
+                return
+            }
+        });
+        // If this position is not yet included, we can add it
+        if (!duplicate) {
+            positions.push({ x: col, y: row })
+        }
+    }
+    return positions
+}
+
+// Marking and unmarking user's choice
+function wordCallback(id) {
+    // Removing previously selected element
+    if (selectedIds.includes(id)) {
+        selectedIds.splice(selectedIds.indexOf(id), 1)
+        document.getElementById("word" + id).style.color = "black"
+    } else {
+        document.getElementById("word" + id).style.color = "#8c92ac"
+        // Adding the word to the selected ones
+        selectedIds.push(id)
+    }
+}
+
+// Checking user's answers and highlighting them accordingly
+function checkAnswers() {
+    let points = 0
+    for (let index in selectedIds) {
+        let id = selectedIds[index]
+        // Highlighting all the right chosen answers
+        if (rightAnswers.includes(allAnswers[id])) {
+            document.getElementById("word" + id).style.color = "#8fbc8f"
+            document.getElementById("word" + id).innerHTML += " <span style='font-size:20px;'>Good</span>"
+            points += 2
+        } else {
+            // Highlighting all the wrong chosen answers
+            document.getElementById("word" + id).style.color = "#cc3333"
+            document.getElementById("word" + id).innerHTML += " <span style='font-size:20px;'>Bad</span>"
+            points -= 1
+        }
+    }
+    // Highlighting all the right answers that weren't chosen
+    for (let index in rightAnswers) {
+        let id = allAnswers.indexOf(rightAnswers[index])
+        if (!selectedIds.includes(id)) {
+            document.getElementById("word" + id).style.color = "#8fbc8f"
+            document.getElementById("word" + id).innerHTML += " <span style='font-size:20px;'>Good</span>"
+            points -= 1
+        }
+    }
+    let button = document.getElementById("answersButton")
+    button.innerHTML = "finish game"
+    button.setAttribute("onclick", "submitAnswers()")
+    document.getElementById("score").innerHTML = points + " points"
+}
+
+// Switching the view from game screen to score screen
 function submitAnswers() {
     let gameScreen = document.getElementById("gameScreen")
     gameScreen.parentNode.removeChild(gameScreen)
